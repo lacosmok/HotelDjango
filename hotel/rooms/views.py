eecab0ef7d131304f14ff8c1402f5a97576ec3ee
Laco Smokowski <laco.smokowski@profil-software.com>
2016-12-08 15:31:21 +0100

    compleated registration, fixes to views and models

--- hotel/rooms/views.py
+++ hotel/rooms/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView, DetailView, View
-from .models import Hotel
+from .models import Hotel, Reservation
 from .forms import UserForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+class ReservationCreateView(CreateView):
+    model = Reservation
+    template_name = "registration_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ReservationCreateView, self).get_context_data(**kwargs)
+        context['reservation_create'] = True
+        return context
+
+    def form_valid(self, form, **kwargs):
+        context = super(ReservationCreateView, self).form_valid(form, **kwargs)
+        form.save()
+        return context
 
 class HotelListView(ListView):
     model = Hotel
@@ -16,4 +33,24 @@ class HotelListView(ListView):
 
 class UserFormView(View):
     form_class = UserForm
-    Template = "registration_form.html"
+    template_name = "registration_form.html"
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user.set_password(password)
+            user.save()
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
